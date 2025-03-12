fad6ea0f30c29b880dd2beea3aa6f4689aba2bb9
Kunal-Vala <kunalvala2005@gmail.com>
2025-03-12 20:49:47 +0530

    Add - Human & Computer Choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-alert("PLAY NOW");
+// alert("PLAY NOW");
+
+function getComputerChoice(){
+    let choice = getRandomInt(3)
+    // console.log(choice)
+    switch(choice){
+        case 0 : return "rock";
+        case 1 : return "paper";
+        case 2 : return "scissors";
+        
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+console.log(getComputerChoice())
+
+let choice = prompt("ROCK | PAPER | SCISSOR");
+function getHumanChoice(choice){
+    choice = choice.toLowerCase();
+    console.log(choice)
+}
+
+getHumanChoice(choice);
